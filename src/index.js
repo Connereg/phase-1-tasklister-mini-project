a8b0c40b762cfa6b28472f3f1ad60fd4b6b20879
Conner Gleason <connereg@optonline.net>
2021-08-16 13:50:02 -0400

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (s) => {
+    s.preventDefault()
+    buildToDoItem(s.target.querySelector('input').value)
+  })
+  
 });
+
+function buildToDoItem(todo){
+  let p = document.createElement('p')
+  let xbtn = document.createElement('button')
+  xbtn.addEventListener("click", (x) => {
+    x.target.parentNode.remove()},
+  //Anonymous function that removes the parent node of the X button variable inserted into 'x' when the Xbtn is clicked
+  
+  xbtn.textContent = " x ",
+  
+  
+  
+  p.textContent = `${todo} `,
+  p.appendChild(xbtn),
+  //Appends Xbtn to the Added to-do list element provided as a submit, will do this for each submit
+
+
+  
+  document.getElementById('tasks').appendChild(p)
+  // Adds the text of the submit to a new item line on To Do list
+  )}
